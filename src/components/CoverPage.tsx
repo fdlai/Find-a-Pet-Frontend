6efd7f885741ec24babc371c27d923fe7b81c590
Fred Lai <fdlai@yahoo.com>
2024-10-27 09:59:39 -0400

    adjust some coverpage styles

--- src/components/CoverPage.tsx
+++ src/components/CoverPage.tsx
@@ -5,17 +5,19 @@ export default function CoverPage() {
   const navigate = useNavigate();
   return (
     <div className="coverpage">
-      <h1 className="coverpage__title">
-        Welcome to <span className="coverpage__accent-text">Find a Pet!</span>
-      </h1>
-      <h2 className="coverpage__text">Begin your search for a pet here</h2>
-      <button
-        className="coverpage__button"
-        type="button"
-        onClick={() => navigate("/main")}
-      >
-        Let's go!
-      </button>
+      <div className="coverpage__content">
+        <h1 className="coverpage__title">
+          Welcome to <span className="coverpage__accent-text">Find a Pet!</span>
+        </h1>
+        <h2 className="coverpage__text">Begin your search for a pet here</h2>
+        <button
+          className="coverpage__button"
+          type="button"
+          onClick={() => navigate("/main")}
+        >
+          Let's go!
+        </button>
+      </div>
     </div>
   );
 }
