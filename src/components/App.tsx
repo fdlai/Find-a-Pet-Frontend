ff5ad96c4b9996854241e69a71931aee9a213abf
Fred Lai <fdlai@yahoo.com>
2024-10-27 09:21:23 -0400

    set up some basic routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,27 @@
 import "../blocks/App.css";
+import CoverPage from "./CoverPage";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="page">
-      <div className="page__content">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<CoverPage />} />
+      <Route
+        path="main"
+        element={
+          <div className="page">
+            <div className="page__content">
+              <Header />
+              <Main />
+            </div>
+            <Footer />
+          </div>
+        }
+      />
+    </Routes>
   );
 }
 
---
+++ src/components/CoverPage.tsx
@@ -0,0 +1,21 @@
+import "../blocks/CoverPage.css";
+import { useNavigate } from "react-router-dom";
+
+export default function CoverPage() {
+  const navigate = useNavigate();
+  return (
+    <div className="coverpage">
+      <h1 className="coverpage__title">
+        Welcome to <span className="coverpage__accent-text">Find a Pet!</span>
+      </h1>
+      <h2 className="coverpage__text">Begin your search for a pet here</h2>
+      <button
+        className="coverpage__button"
+        type="button"
+        onClick={() => navigate("/main")}
+      >
+        Let's go!
+      </button>
+    </div>
+  );
+}
